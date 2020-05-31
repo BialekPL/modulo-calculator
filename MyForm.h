3c4a78fec2862bed823c54654b94182fdca2f3d4
BialekPL <mbialecki157@gmail.com>
2020-05-31 15:35:17 +0200

    added buttons and labels

--- MyForm.h
+++ MyForm.h
@@ -34,6 +34,59 @@ namespace modulocalculator {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^ buttonMinus;
+	protected:
+
+	protected:
+
+	private: System::Windows::Forms::Button^ button7;
+	private: System::Windows::Forms::Button^ button8;
+	protected:
+
+
+
+	protected:
+
+
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button6;
+
+	private: System::Windows::Forms::Button^ button5;
+
+
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ buttonTimes;
+
+
+
+
+	public: System::Windows::Forms::Button^ button0;
+	private: System::Windows::Forms::Button^ button9;
+	private: System::Windows::Forms::Button^ buttonDivided;
+	public:
+	private:
+
+	private:
+
+
+
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ buttonPlus;
+
+
+
+
+
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ buttonEquals;
+
+
+
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label2;
+
 
 	private:
 		/// <summary>
@@ -48,12 +101,250 @@ namespace modulocalculator {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->buttonMinus = (gcnew System::Windows::Forms::Button());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->button8 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->buttonTimes = (gcnew System::Windows::Forms::Button());
+			this->button0 = (gcnew System::Windows::Forms::Button());
+			this->button9 = (gcnew System::Windows::Forms::Button());
+			this->buttonDivided = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->buttonPlus = (gcnew System::Windows::Forms::Button());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->buttonEquals = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// buttonMinus
+			// 
+			this->buttonMinus->Location = System::Drawing::Point(174, 286);
+			this->buttonMinus->Name = L"buttonMinus";
+			this->buttonMinus->Size = System::Drawing::Size(50, 50);
+			this->buttonMinus->TabIndex = 0;
+			this->buttonMinus->Text = L"-";
+			this->buttonMinus->UseVisualStyleBackColor = true;
+			// 
+			// button7
+			// 
+			this->button7->Location = System::Drawing::Point(5, 174);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(50, 50);
+			this->button7->TabIndex = 1;
+			this->button7->Text = L"7";
+			this->button7->UseVisualStyleBackColor = true;
+			// 
+			// button8
+			// 
+			this->button8->Location = System::Drawing::Point(61, 174);
+			this->button8->Name = L"button8";
+			this->button8->Size = System::Drawing::Size(50, 50);
+			this->button8->TabIndex = 2;
+			this->button8->Text = L"8";
+			this->button8->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(5, 230);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(50, 50);
+			this->button4->TabIndex = 3;
+			this->button4->Text = L"4";
+			this->button4->UseVisualStyleBackColor = true;
+			this->button4->Click += gcnew System::EventHandler(this, &MyForm::button4_Click);
+			// 
+			// button6
+			// 
+			this->button6->Location = System::Drawing::Point(117, 230);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(50, 50);
+			this->button6->TabIndex = 4;
+			this->button6->Text = L"6";
+			this->button6->UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(61, 230);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(50, 50);
+			this->button5->TabIndex = 5;
+			this->button5->Text = L"5";
+			this->button5->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(61, 286);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(50, 50);
+			this->button2->TabIndex = 6;
+			this->button2->Text = L"2";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button7_Click);
+			// 
+			// buttonTimes
+			// 
+			this->buttonTimes->Location = System::Drawing::Point(173, 174);
+			this->buttonTimes->Name = L"buttonTimes";
+			this->buttonTimes->Size = System::Drawing::Size(50, 50);
+			this->buttonTimes->TabIndex = 7;
+			this->buttonTimes->Text = L"*";
+			this->buttonTimes->UseVisualStyleBackColor = true;
+			// 
+			// button0
+			// 
+			this->button0->Location = System::Drawing::Point(61, 342);
+			this->button0->Name = L"button0";
+			this->button0->Size = System::Drawing::Size(50, 50);
+			this->button0->TabIndex = 8;
+			this->button0->Text = L"0";
+			this->button0->UseVisualStyleBackColor = true;
+			// 
+			// button9
+			// 
+			this->button9->Location = System::Drawing::Point(118, 174);
+			this->button9->Name = L"button9";
+			this->button9->Size = System::Drawing::Size(50, 50);
+			this->button9->TabIndex = 9;
+			this->button9->Text = L"9";
+			this->button9->UseVisualStyleBackColor = true;
+			// 
+			// buttonDivided
+			// 
+			this->buttonDivided->Location = System::Drawing::Point(173, 118);
+			this->buttonDivided->Name = L"buttonDivided";
+			this->buttonDivided->Size = System::Drawing::Size(50, 50);
+			this->buttonDivided->TabIndex = 10;
+			this->buttonDivided->Text = L"/";
+			this->buttonDivided->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(118, 286);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(50, 50);
+			this->button3->TabIndex = 11;
+			this->button3->Text = L"3";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// buttonPlus
+			// 
+			this->buttonPlus->Location = System::Drawing::Point(173, 230);
+			this->buttonPlus->Name = L"buttonPlus";
+			this->buttonPlus->Size = System::Drawing::Size(50, 50);
+			this->buttonPlus->TabIndex = 12;
+			this->buttonPlus->Text = L"+";
+			this->buttonPlus->UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(5, 286);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(50, 50);
+			this->button1->TabIndex = 13;
+			this->button1->Text = L"1";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// buttonEquals
+			// 
+			this->buttonEquals->Location = System::Drawing::Point(118, 342);
+			this->buttonEquals->Name = L"buttonEquals";
+			this->buttonEquals->Size = System::Drawing::Size(105, 50);
+			this->buttonEquals->TabIndex = 14;
+			this->buttonEquals->Text = L"=";
+			this->buttonEquals->UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 25, System::Drawing::FontStyle::Bold));
+			this->label1->Location = System::Drawing::Point(5, 11);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(39, 39);
+			this->label1->TabIndex = 15;
+			this->label1->Text = L"Z";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12));
+			this->textBox1->Location = System::Drawing::Point(40, 23);
+			this->textBox1->MaximumSize = System::Drawing::Size(50, 30);
+			this->textBox1->MinimumSize = System::Drawing::Size(28, 30);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->ScrollBars = System::Windows::Forms::ScrollBars::Both;
+			this->textBox1->Size = System::Drawing::Size(41, 30);
+			this->textBox1->TabIndex = 16;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::textBox1_TextChanged);
+			// 
+			// textBox2
+			// 
+			this->textBox2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 28));
+			this->textBox2->Location = System::Drawing::Point(5, 118);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->ReadOnly = true;
+			this->textBox2->Size = System::Drawing::Size(162, 50);
+			this->textBox2->TabIndex = 17;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(2, 89);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(35, 13);
+			this->label2->TabIndex = 18;
+			this->label2->Text = L"label2";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->AutoSize = true;
+			this->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
+			this->ClientSize = System::Drawing::Size(234, 395);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->buttonEquals);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->buttonPlus);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->buttonDivided);
+			this->Controls->Add(this->button9);
+			this->Controls->Add(this->button0);
+			this->Controls->Add(this->buttonTimes);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button5);
+			this->Controls->Add(this->button6);
+			this->Controls->Add(this->button4);
+			this->Controls->Add(this->button8);
+			this->Controls->Add(this->button7);
+			this->Controls->Add(this->buttonMinus);
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->Name = L"MyForm";
+			this->Text = L"Modulo calculator";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button7_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void button4_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
